c88d3aa80a37d2b8fad1fb2e51cd8f4a2cd08a8c
Edward Webber <gitcommit@webbsense.com>
2020-02-10 21:13:15 -0600

    Fix for VS code intellisense

--- buildroot/scripts/debug_filename.py
+++ buildroot/scripts/debug_filename.py
@@ -4,8 +4,10 @@ Import("env")
 print("Creating debug-compatible firmware.elf link")
 if os.path.exists('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.elf"):
     os.unlink('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.elf")
-os.link('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + env["PROGNAME"] + ".elf",'D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.elf")
+if os.path.exists('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + env["PROGNAME"] + ".elf"):
+    os.link('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + env["PROGNAME"] + ".elf",'D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.elf")
 print("Creating debug-compatible firmware.bin link")
 if os.path.exists('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.bin"):
     os.unlink('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.bin")
-os.link('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + env["PROGNAME"] + ".bin",'D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.bin")
+if os.path.exists('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + env["PROGNAME"] + ".bin"):
+    os.link('D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + env["PROGNAME"] + ".bin",'D:/Google Drive/Development/GitHub/BIGTREETECH-TouchScreenFirmware/.pio/build/BIGTREE_TFT35_V2_0/' + "firmware.bin")
