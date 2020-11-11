4812d38c17225e52241f6bafa637813127c26f95
Edward Webber <gitcommit@webbsense.com>
2020-11-11 16:29:50 -0600

    Includes overhaul - Renamed duplicate menu function - Fixed invalid color variable name - All files only include necessary files

---
+++ TFT/src/User/scratch.c
@@ -0,0 +1,91 @@
+// C helper classes
+#include "System/variants.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include "API/helperFunctions.h"
+#include "API/System/init.h"
+
+// Timing functions
+#include "System/os_timer.h"
+#include "System/delay.h"
+#include "System/boot.h"
+
+// Multi-language support
+#include "Language/utf8_decode.h"
+#include "Language/Language.h"
+
+// LCD init functions
+#include "lcd.h"
+#include "Hal/LCD_Init.h"
+#include "lcd_dma.h"
+#include "UI/GUI.h"
+#include "GPIO_Init.h"
+
+// Chip specific includes
+#include "usart.h"
+#include "Serial.h"
+#include "spi.h"
+#include "sw_spi.h"
+#include "spi_slave.h"
+
+// USB drive support (select TFT models)
+#include "usbh_core.h"
+#include "usbh_usr.h"
+#include "usbh_msc_core.h"
+
+// SD card support
+#include "Hal/sd.h"
+#include "Hal/w25qxx.h"
+#include "Hal/xpt2046.h"
+
+// File handling
+#include "System/LCD_Encoder.h"
+#include "ST7920_Simulator.h"
+#include "Fatfs/ff.h"
+#include "Vfs/vfs.h"
+#include "Fatfs/myfatfs.h"
+
+// Gcode processing
+#include "Gcode/gcodeSender.h"
+#include "Gcode/gcodeRequests.h"
+#include "Gcode/gcodeFileHandler.h"
+#include "Gcode/gcodeResponseHandler.h"
+
+// Base API functions
+#include "API/coordinate.h"
+#include "API/gantry.h"
+#include "API/menu.h"
+#include "API/extendedFeatures.h"
+#include "API/ledcolor.h"
+
+// UI handling
+#include "UI/ui_draw.h"
+#include "UI/list_item.h"
+#include "UI/touch_process.h"
+#include "System/flashStorage.h"
+#include "System/emulationToggle.h"
+
+// Menus
+#include "Menu/menuMain.h"  // Home screen
+#include "Menu/menuMove.h"
+#include "Menu/menuBabystepping.h"
+#include "Menu/menuFileSelect.h"
+#include "Menu/menuHoming.h"
+#include "Menu/menuJobSetup.h"
+#include "Menu/menuJobStatus.h"
+#include "Menu/menuPause.h"
+#include "Menu/menuPopup.h"
+#include "Menu/menuPower.h"
+#include "Menu/menuRouter.h"
+#include "Menu/menuSettingsFeatures.h"
+#include "Menu/menuSettingsHome.h"
+#include "Menu/menuSettingsMisc.h"
+#include "Menu/menuSettingsTFT.h"
+#include "Menu/menuSettingsTMC.h"
+#include "Menu/menuSpeed.h"
+#include "Menu/menuTerminal.h"
+
